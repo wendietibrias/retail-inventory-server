73ff4a9a8f194cb3275e67e6c8239251a2904073
wendietibrias <wendietibrias@gmail.com>
2025-12-05 14:59:47 +0700

    chore: Major Update

--- app/Http/Controllers/V1/DashboardController.php
+++ app/Http/Controllers/V1/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\SalesInvoice;
+use App\Models\ShiftTransaction;
 use App\Models\TransactionSummarize;
 use Carbon\Carbon;
 use DB;
@@ -24,14 +26,25 @@ class DashboardController extends Controller
         ]);
 
         try {
+            $now = Carbon::now();
+
             /**
              *  Total Sales
              */
 
-            $now = Carbon::now();
-
+            $latestSalesInvoice = SalesInvoice::whereDate('created_at', $now)->take(6)->get();
+            $latestShiftTransaction = ShiftTransaction::whereDate('created_at', $now)->take(6)->get();
             $transactionSummarize = DB::table('transaction_summarize')
-                ->select(['whole_total', 'retail_total', 'online_total', 'dealer_total', 'showcase_total', 'ppn_total', 'non_ppn_total'], )
+                ->select('ppn_total', DB::raw('SUM(ppn_total) as totalPpn'))
+                ->select('non_ppn_total', DB::raw('SUM(non_ppn_total) as totalNonPpn'))
+                ->select('dealer_total', DB::raw('SUM(dealer_total) as totalDealer'))
+                ->select('showcase_total', DB::raw('SUM(showcase_total) as totalShowcase'))
+                ->select('online_total', DB::raw('SUM(online_total) as totalOnline'))
+                ->select('retail_total', DB::raw('SUM(retail_total) as totalRetail'))
+                ->select('whole_total', DB::raw('SUM(whole_total) as totalWhole'))
+                ->select('item_total', DB::raw('SUM(item_total) as totalItem'))
+                ->select('big_item_total', DB::raw('SUM(big_item_total) as totalBigItem'))
+                ->select('leasing_item_total', DB::raw('SUM(leasing_item_total) as totalLeasingItem'))
                 ->where('deleted_at', null);
 
             if ($request->has('type')) {
@@ -44,6 +57,7 @@ class DashboardController extends Controller
                 }
                 if ($type === 'month') {
                     $transactionSummarize->whereMonth('created_at', $request->get('month'));
+                    $transactionSummarize->groupBy('');
                 }
 
                 if ($type === 'year') {
@@ -52,12 +66,15 @@ class DashboardController extends Controller
             }
 
 
-
             $data = [];
 
-            $data['transaction_summarize_details'] = $transactionSummarize->get();
+            $data['latest_shift_transaction'] = $latestShiftTransaction;
+            $data['latest_sales_invoice'] = $latestSalesInvoice;
+            $data['transaction_summarize'] = $transactionSummarize->get();
 
-
+            return $this->successResponse("Berhasil Mendapatkan Data Dashboard", [
+                'data' => $data
+            ]);
 
         } catch (Exception $e) {
             return $this->errorResponse($e->getMessage(), $e->getCode(), []);
